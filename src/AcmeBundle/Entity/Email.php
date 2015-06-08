648e55a662e1384e9feee74a544ff5513e3dfdbb
Ken Marfilla <marfillaster@gmail.com>
2015-06-08 20:22:49 +0800

    create email entity

---
+++ src/AcmeBundle/Entity/Email.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace AcmeBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Email
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Email
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="is_primary", type="boolean")
+     */
+    private $isPrimary;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Contact")
+     * @ORM\JoinColumn(name="contact_id", referencedColumnName="id")
+     **/
+    private $contact;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return Email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set isPrimary
+     *
+     * @param boolean $isPrimary
+     * @return Email
+     */
+    public function setIsPrimary($isPrimary)
+    {
+        $this->isPrimary = $isPrimary;
+
+        return $this;
+    }
+
+    /**
+     * Get isPrimary
+     *
+     * @return boolean
+     */
+    public function getIsPrimary()
+    {
+        return $this->isPrimary;
+    }
+
+    /**
+     * Set contact
+     *
+     * @param \stdClass $contact
+     * @return Email
+     */
+    public function setContact($contact)
+    {
+        $this->contact = $contact;
+
+        return $this;
+    }
+
+    /**
+     * Get contact
+     *
+     * @return \stdClass
+     */
+    public function getContact()
+    {
+        return $this->contact;
+    }
+}
