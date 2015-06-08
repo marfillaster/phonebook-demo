abdca21e5164e0c3bbcacf524024a8b2e9c7e808
Ken Marfilla <marfillaster@gmail.com>
2015-06-08 14:18:44 +0800

    create contact entity and repository

---
+++ src/AcmeBundle/Entity/Contact.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace AcmeBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="ContactRepository")
+ * @ORM\Table(name="contact")
+ */
+class Contact
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Name is required")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=12)
+     */
+    private $mobile;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Contact
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set mobile
+     *
+     * @param string $mobile
+     * @return Contact
+     */
+    public function setMobile($mobile)
+    {
+        $this->mobile = $mobile;
+
+        return $this;
+    }
+
+    /**
+     * Get mobile
+     *
+     * @return string
+     */
+    public function getMobile()
+    {
+        return $this->mobile;
+    }
+}
