7d6b7330937150fcab6bde15302887dd30febd37
Ken Marfilla <marfillaster@gmail.com>
2015-06-15 01:02:06 +0800

    mount bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,10 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+            new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
+
+            new FOS\RestBundle\FOSRestBundle(),
             new AcmeBundle\AcmeBundle(),
             new ApiBundle\ApiBundle(),
         );
